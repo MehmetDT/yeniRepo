d992cdc110ae815c975682f11c9ccc477dac7d0b
mehmet doğukan taban <mtaban24@gmail.com>
2022-11-22 00:21:25 +0300

    App.jsde düzenleme yaptım

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text>Ok</Text>
+        <Text>FEAFAESDGASFDSADSA</Text>
       </View>
     );
   }
