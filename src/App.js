a1e3e1f873253895e1eee1bcd0972dd91e4757a1
08rauf <raufhasan08@gmail.com>
2022-11-22 16:28:38 +0300

    bi bak buna

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  TextInput,
+  SafeAreaView,
+} from 'react-native';
 
-export default class App extends Component {
+export default class About extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
+    this.state = {};
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>heeeeeeeeee</Text>
-        <Text>kangallllll</Text>
-        <Text> MTD </Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <TouchableOpacity style={styles.bckcntnr}>
+          <Image
+            source={require('./assets/images/back.png')}
+            style={styles.back}
+          />
+        </TouchableOpacity>
+
+        <Text style={styles.tyab}> Tell your about </Text>
+        <TextInput placeHolder/>
+        <TouchableOpacity>
+          <Text>Continue</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     );
   }
 }
@@ -25,6 +40,20 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+  },
+  bckcntnr: {
+    width: 375,
+    height: 92,
+    justifyContent: 'center',
+    marginLeft: 20,
+  },
+  back: {
+    width: 40,
+    height: 40,
+  },
+  tyab:{
+    fontSize:24,
+    fontWeight:'bold',
+    color:'#4B164C'
   }
-})
+});
---
+++ src/screens/Splash.js
@@ -0,0 +1,38 @@
+import React, {Component,} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  ImageBackground,
+} from 'react-native';
+
+export default class Splash extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <ImageBackground
+        source={require('./assets/images/bckgrnd.png')}
+        style={styles.bckgrnd}>
+        <Image source={require('./assets/images/migo.png')} style={styles.migo} />
+      </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bckgrnd: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  migo: {
+    width: 156,
+    height: 43,
+  },
+});
